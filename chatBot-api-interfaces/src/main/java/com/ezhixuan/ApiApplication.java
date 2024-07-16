83e0cef68cf708b1efa6cb1a94fdc6e7b2484603
Ezhixuan <ezhixuanna@163.com>
2024-07-16 21:27:51 +0800

    feat: init
    - project init
    
    Signed-off-by: Ezhixuan <ezhixuanna@163.com>

---
+++ chatBot-api-interfaces/src/main/java/com/ezhixuan/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.ezhixuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ApiApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
